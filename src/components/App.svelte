38fc5f1fb4ab9bdb2ec1aaee3c2eea4676a82106
Sergey Sokurenko <s.sokurenko@gmail.com>
2019-04-21 17:43:44 +0200

    Gettigng search results by AJAX call and isplaying at the grid

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,10 +1,60 @@
+
+
 <main class="app-main app-theme">
 	<div class="app-container">
 		<div class="app-title">
 			{ title }
 		</div>
+		<div class="app-search">
+			<input placeholder="Search query..." class="app-search-input app-theme" bind:value=searchQuery>
+			<button class="app-search-button app-theme" on:click="onSearch(searchQuery)">
+				Search
+			</button>
+		</div>
+		{#if response}
+			<Results data={{response}} />
+		{/if}
 	</div>
+	
 </main>
+
+<script>
+
+import Results from './Results.svelte';
+
+const fetchResults = async (data, component, query) => {
+  const r = await fetch(`https://api.altoshift.com/v1/search?query=${query}&token=5c4597b15608d4002e09d979-1548064689054-26650276`);
+	const json = await r.json();
+  const response = json;
+  component.set({ response });
+	data.response = Object.assign({}, json);
+};
+
+export default {
+	oncreate() {
+		let data = this.get();
+		this.set({searchQuery: ''});
+	},
+	methods: {
+		search(query) {
+			let data = this.get();
+			fetchResults(data, this, query);
+		},
+
+		onSearch() {
+			let data = this.get();
+			if (data.searchQuery && data.searchQuery.length > 1) {
+				this.search(data.searchQuery);
+			}
+		}
+	},
+	components: {
+		Results
+	}
+};
+
+
+</script>
 
 <style>
 	.app-main {
@@ -33,5 +83,30 @@
 
 	.app-title {
 		font-size: 2rem;
+	}
+
+	.app-search {
+		padding: 22px 0;
+		text-align: center;
+	}
+
+	.app-search-input {
+		padding: 12px;
+    font-size: 1rem;
+    border-radius: 4px 0 0 4px;
+		border: 1px solid lightgrey;
+		outline: none;
+	}
+
+	.app-search-button {
+		cursor: pointer;
+    font-size: 1rem;
+    font-weight: 700;
+    padding: 13px;
+    background: lightgray;
+    border: none;
+    margin-left: -10px;
+		border-radius: 0 4px 4px 0;
+		outline: none;
 	}
 </style>
---
+++ src/components/Results.svelte
@@ -0,0 +1,39 @@
+<div class="app-results">
+  <strong>Found: {data.response.total}</strong>
+
+  <div class="app-products">
+    {#each data.response.products as product}
+      <Product data={{product}} />
+    {:else}
+      <p>
+        Please try another search query.
+      </p>
+    {/each}
+  </div>
+</div>
+
+<script>
+import Product from './Product.svelte';
+export default {
+	oncreate() {
+		let data = this.get();
+	},
+	components: {
+    Product
+  }
+};
+</script>
+
+<style>
+.app-results {
+  text-align: center;
+}
+
+.app-products {
+  text-align: center;
+  height: 100%;
+  max-height: 500px;
+  overflow-y: scroll;
+  margin-top: 16px;
+}
+</style>
