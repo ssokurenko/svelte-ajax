383ffa0f2b417837c80202d1b1eb39983fbb2ad6
Sergey Sokurenko <s.sokurenko@gmail.com>
2019-04-22 12:55:24 +0200

    Minor optimization

--- public/app-bootstrap.js
+++ public/app-bootstrap.js
@@ -1,17 +1,17 @@
 (function(d) {
-  var container = document.createElement('div');
+  var container = d.createElement('div');
   container.id = 'svelte-container';
   d.body.appendChild(container);
 
-  var script = document.createElement('script');
+  var script = d.createElement('script');
   script.type = 'text/javascript';
   script.src = '//svelte-ajax.surge.sh/app.js';
   d.body.appendChild(script);
 
-  var theme = document.createElement('link');
+  var theme = d.createElement('link');
   theme.rel = 'stylesheet';
   theme.type = 'text/css';
   theme.href = '//svelte-ajax.surge.sh/themes/blue.css';
-  d.body.appendChild(theme)
+  d.body.appendChild(theme);
 
 }(document));
